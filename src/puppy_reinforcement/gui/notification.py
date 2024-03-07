1c1e9f392559faf56b0b51c9f0d4a2f9baef546d
Jason Gao <jgao222@uw.edu>
2024-03-06 19:22:11 -0800

    build

--- src/puppy_reinforcement/gui/notification.py
+++ src/puppy_reinforcement/gui/notification.py
@@ -100,7 +100,7 @@ class Notification(QLabel):
             # size.setWidth(image_height * aspect)
             # manual scaling since methods w/ aspect ratio specified didn't work
             # size.scale(image_height, image_height, Qt.AspectRatioMode.KeepAspectRatio)
-            size.scale(1024, 1024, Qt.AspectRatioMode.KeepAspectRatio)
+            size.scale(1024, 1024, Qt.AspectRatioMode.KeepAspectRatioByExpanding)
             movie.setScaledSize(size)
             movie_label = QLabel()
             # movie_label.setMaximumHeight(image_height)
